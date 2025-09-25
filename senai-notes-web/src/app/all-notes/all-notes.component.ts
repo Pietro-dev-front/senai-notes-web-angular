451632b331228633550e8dfcd258bce62b9a6313
Lucas Melo <lucasmelo4606@gmail.com>
2025-09-24 21:48:41 -0300

    Add AllNotes component and update routes and user module templates

---
+++ senai-notes-web/src/app/all-notes/all-notes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-notes',
+  imports: [],
+  templateUrl: './all-notes.component.html',
+  styleUrl: './all-notes.component.scss'
+})
+export class AllNotesComponent {
+
+}
