e5a16c65886d8c3ab12d66cc973c2eca6eb49359
Pietro Henrique <pietrohenriquealvesdossantospi@gmail.com>
2025-10-02 21:49:36 -0300

    feat: database

--- src/app/all-notes/all-notes.component.ts
+++ src/app/all-notes/all-notes.component.ts
@@ -1,11 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-all-notes',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './all-notes.component.html',
   styleUrl: './all-notes.component.scss'
 })
 export class AllNotesComponent {
+   imageUrl: string | ArrayBuffer | null = null;
 
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.imageUrl = reader.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
 }
