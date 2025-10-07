ace6c7eb401e53486cd02c1ad720d531347f096b
Pietro Henrique <pietrohenriquealvesdossantospi@gmail.com>
2025-10-06 21:48:46 -0300

    feat : integracao

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -3,14 +3,20 @@ import { Router } from "@angular/router"
 
 export const authGuard = () => {
     const router = inject(Router);
-    const token = localStorage.getItem("meuToken")
-    const userId = localStorage.getItem("meuId")
-
-    if (token != null && userId != null) {
-        return true;
-    } else {
+    // Protege acesso a localStorage em ambiente SSR
+    if (typeof window === 'undefined') {
         router.navigate(["/login"]);
         return false;
     }
+
+    const token = localStorage.getItem("meuToken");
+    const userId = localStorage.getItem("meuId");
+
+    if (token != null && userId != null) {
+        return true;
+    }
+
+    router.navigate(["/login"]);
+    return false;
 
 }
