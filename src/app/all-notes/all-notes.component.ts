d24c6ccc1d6c815f31e764612dae0ff55c41b0df
Lucas Melo <lucasmelo4606@gmail.com>
2025-10-06 21:54:17 -0300

    config page all notes - imgs

--- src/app/all-notes/all-notes.component.ts
+++ src/app/all-notes/all-notes.component.ts
@@ -2,21 +2,27 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-all-notes',
+  standalone: true,
   imports: [],
   templateUrl: './all-notes.component.html',
-  styleUrl: './all-notes.component.scss'
+  styleUrls: ['./all-notes.component.scss']
 })
 export class AllNotesComponent {
-  darkMode: boolean = false
-  
-  ligarDesligarDarkMode() {
+  darkMode: boolean = false;
+  imageUrl: string | null = null;
 
-    this.darkMode = !this.darkMode; // o inverso do this.darkmode .
-  
+  ligarDesligarDarkMode() {
+    this.darkMode = !this.darkMode;
     document.body.classList.toggle("dark-mode", this.darkMode);
-    
     localStorage.setItem("darkMode", this.darkMode.toString());
   }
 
-}
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
 
+    if (input.files && input.files[0]) {
+      const file = input.files[0];
+      this.imageUrl = URL.createObjectURL(file);
+    }
+  }
+}
